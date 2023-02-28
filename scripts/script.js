8d72200b818ada1819b1a31db2ecb4b62fc2c0cd
Jun Noiri <63932655+junnoiri@users.noreply.github.com>
2023-02-27 21:23:16 -0600

    updated some contents

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,28 @@
 $(document).ready(function () {
-  console.log("doc is ready");
-
-  let revName;
-
   $("#btnGetFacts").click(function () {
-    // handle it if input is blank
-
     let userName = $("#userName").val();
-    let coolFacts = generateCoolNameFacts(userName);
-
-    console.log("you clicked the button");
-    console.log("from ln 12", generateCoolNameFacts(userName));
-
-    $("#coolNameFactsOutput").html(coolFacts);
+    let nameLength = generateNameFacts(userName);
+    $("#outputDiv").html(nameLength);
   });
 });
 
-function generateCoolNameFacts(userName) {
-  let coolFacts = "";
-
-  coolFacts = nameLength(userName);
-  coolFacts += " <br> " + nameStart(userName);
-  coolFacts += "<h3> " + nameEnd(userName) + "</h3>";
-  coolFacts += "<h2>" + reverseName(userName) + "</h2>";
-
-  console.log("initialized the'm coolFacts variable: " + coolFacts);
-
-  return coolFacts;
+function generateNameFacts(userName) {
+  let nameLength = "";
+  nameLength = getNameLength(userName);
+  nameLength += " <div> " + getFirstLetter(userName) + "</div>";
+  nameLength += "<div> " + getLastLetter(userName) + "</div>";
+  return nameLength;
 }
 
-function nameLength(name) {
+function getNameLength(name) {
   return "Your name is " + name.length + " letters long!";
 }
 
-function nameStart(name) {
-  return "the first letter of your name is " + name[0];
+function getFirstLetter(name) {
+  return "The first letter of your name is " + name[0];
 }
 
-function nameEnd(name) {
+function getLastLetter(name) {
   let lastIndex = name.length - 1;
-
-  return "the last letter of your name is " + name[lastIndex];
-}
-
-function reverseName(name) {
-  let arrName = name.split();
-
-  revName = arrName.reverse();
+  return "The last letter of your name is " + name[lastIndex];
 }
